e0897b5059a503ce6a10dd1b378cbd1f2e537805
Sebastian <sbitashenaol@gmail.com>
2019-09-15 21:27:05 -0500

    ajuste de roun robin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
 
   listProceso: any = []
   duracion: string
+  tiempoq: string
   id: number = 1
   tiempoRetorno: number = 0
   tiempoEspera: number = 0
@@ -25,11 +26,14 @@ export class AppComponent {
   tamLista: number = 0
   contadorPrioridad: number = 0
   listProcesoPrioridad: any = []
+  tiempoProceso : number = 0
+  frenarProceso: boolean = false
 
   ngOnInit() {
     $('.inputDuracion').keypress(function(tecla) {
       if (tecla.charCode < 48 || tecla.charCode > 57) return false
     });
+    $('.roundRobin').hide()
   }
 
   pushProceso(valor: any, llenado: boolean) {
@@ -150,12 +154,62 @@ export class AppComponent {
   }
 
   RoundRobin() {
+    this.inhabilitarBotones()
+    $('.roundRobin').show('slow')
+  }
 
+  RoundRobinProceso(q: any, limpiar: boolean) {
+
+    if(limpiar) {
+      this.listProceso = []
+      this.procesoService.getProceso().setPromedioEspera(0)
+      this.procesoService.getProceso().setPromedioRetorno(0)
+    }
+
+    let entrar = true
+
+    // if(q != undefined && q != null && q != '') {
+    //   if (this.procesoService.getProceso().getListOriginal().length > 0) {
+    //     for (var i of this.procesoService.getProceso().getListOriginal()) {
+    //       if(q > i.duracion) {
+    //         entrar = false
+    //       }
+    //     }
+    //   }
+    // }
+
+    if(entrar) {
+      if (this.procesoService.getProceso().getListOriginal().length > 0) {
+        for (var i of this.procesoService.getProceso().getListOriginal()) {
+
+          if(i.tiempoEspera != 0) {
+            i.tiempoEsperaTemp = i.tiempoEspera - q
+          }
+          else {
+            i.tiempoEsperaTemp = 0
+            i.listo = true
+          }
+
+          
+
+          console.log(i.tiempoEsperaTemp);
+          
+          this.procesoService.getProceso().setPromedioEspera(this.procesoService.getProceso().getPromedioEspera() + i.tiempoEspera)
+          this.procesoService.getProceso().setPromedioRetorno(this.procesoService.getProceso().getPromedioRetorno() + i.tiempoRetorno)
+          this.listProceso.push(i)
+        }
+        this.procesoService.getProceso().setPromedioEspera(this.procesoService.getProceso().getPromedioEspera() / this.procesoService.getProceso().getListOriginal().length)
+        this.procesoService.getProceso().setPromedioRetorno(this.procesoService.getProceso().getPromedioRetorno() / this.procesoService.getProceso().getListOriginal().length)
+      }
+    } 
+
+    // this.RoundRobinProceso(q, false)
   }
 
   clear() {
     this.listProceso = []
     this.duracion = ''
+    this.tiempoq = ''
     this.id = 1
     this.tiempoRetorno = 0
     this.tiempoEspera = 0
@@ -167,6 +221,7 @@ export class AppComponent {
     setTimeout(() => {
       $('.tablaProcesos').find('tr').removeClass('activo');
       $('.btn-info, .inputDuracion, .btn-success').removeAttr('disabled');
+      $('.roundRobin').hide('slow')
     }, 100)
   }
 
